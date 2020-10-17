afbd45dbc5a43a006e225144a058d3e90aa48b8b
alide123321 <alide12332@gmail.com>
2020-10-17 23:06:40 +0300

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import sys
 
 
-f = open("bee movie script.txt", "r")
+f = open("bee movie script.txt", "r")  # loads the movie script in f
 
 browser = webdriver.Chrome()  # starts the chromedriver.exe
 browser.get(('https://web.whatsapp.com/'))  # sets link and opens chrome
@@ -16,7 +16,7 @@ time.sleep(15)
 try:
     textbox = browser.find_element_by_xpath(
         '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')  # finds text bot
-except:
+except:  # if error happens
     print("Could'nt find the text box please scan the QR code and click on the user you want to spam")
     browser.get(
         ('https://github.com/alide123321/Whatsapp-bee-movie-spam/blob/main/README.md'))
@@ -24,6 +24,6 @@ except:
 
 
 for x in f:
-    textbox.send_keys(x + "\n")
+    textbox.send_keys(x + "\n")  # prints eatch line in f
 
-textbox.send_keys("Sry bud")
+textbox.send_keys("Sry bud")  # says sorry after it finishs the entire script
